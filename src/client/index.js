7b505218820330162608ca7d144c767b75fa2f5e
Keith Cirkel <keithamus@users.noreply.github.com>
2018-06-07 10:59:51 +0100

    feat: allow permalinking of search terms

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ const debounceTime = 300
 const defaultFilterFunction = (node, input) => Boolean(node.data.contents && new RegExp(input).test(node.data.contents))
 
 document.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.hash.slice(1))
   const idealHeight = Math.min(window.innerHeight - (document.body ? document.body.clientHeight : 0), minSize)
   const graph = new Sunburst({ selector: '#graph', width: minSize, height: idealHeight }, window.data)
   document.addEventListener('resize', () => {
@@ -14,8 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const filterFunction = window.filterFunction || defaultFilterFunction
   const searchInput = document.getElementById('search')
   if (searchInput instanceof HTMLInputElement) {
-    let time = Date.now()
-    searchInput.addEventListener('input', () => {
+    let time = 0
+    const searchGraph = () => {
       if (Date.now() - time < debounceTime) {
         return
       }
@@ -23,7 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
       graph.hideStats()
       graph.updateBreadcrumbs([])
       graph.highlightNodes((node) => filterFunction(node, searchInput.value))
-    })
+    }
+    if (params.get('search')) {
+      searchInput.value = params.get('search')
+      searchGraph()
+    }
+    searchInput.addEventListener('input', searchGraph)
   }
 })
 
